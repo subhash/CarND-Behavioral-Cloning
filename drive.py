8109b7adb3d92b72bc0a33f8d120978d6ba8bec5
subhash <subhashgo@gmail.com>
2017-02-17 11:47:48 +0530

    Runs through first track; stolen model; ranges between -0.5 and 0.5

--- drive.py
+++ drive.py
@@ -61,7 +61,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
-        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2YUV)[:,:,0:1]
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2YUV)
         print(image_array.shape)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
